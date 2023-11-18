11b76fa064ec0b2bb60a4c8d80b740b1424b6d73
oco14 <omarocruz1@gmail.com>
2023-11-18 14:43:06 -0600

    Update to true_false_conversion. Commentation was also added

--- true_false_conversion.py
+++ true_false_conversion.py
@@ -1,15 +1,20 @@
 import pandas as pd
 
-
-def convert_bool_values_to_int(df):
+# function to convert boolean values to int values by checking the elements of 
+# the 'Verified" column
+def convert_bool_values_to_int(df): 
+    # replaces original bool val to type int
     df['Verified'] = df['Verified'].astype(int)
+    # returns the modifed data frame value 
     return df
 
-file_path = 'bot_detection_data_copy.csv'
+# reads the .csv file into the data frame
+df = pd.read_csv('bot_detection_data_copy.csv') 
 
-df = pd.read_csv('bot_detection_data_copy.csv')
+# sends the data frame into the function 'convert_bool_values_to_int
+df = convert_bool_values_to_int(df)  
 
-df = convert_bool_values_to_int(df)
-
-df.to_csv('modded_detection_data.csv', index = False)
+# creates a new .csv file, 'modded_detection_data,' containing the data frame after being
+# modified by convert_bool_values_to_int 
+df.to_csv('modded_detection_data.csv', index = False)  
 
